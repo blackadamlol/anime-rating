5d9846415accec5b2825be63bc0653475f5cfb54
blackadamlol <ssahilpreet35@gmail.com>
2023-07-09 11:45:20 +0100

    rating framework done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import Cards from './component/Cards';
+import naruto from "./images/narutoo.jpg"
+import onepiece from "./images/one-p.jpg"
+import bleach from "./images/bleach.jpg"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Cards
+        imgsrc={naruto}
+        imgalt="Naruto Image"
+        title="Anime"
+        name="Naruto"
+        link="www.zoro.bz" 
+      />
+
+      <Cards
+        imgsrc={onepiece}
+        imgalt="one piece Image"
+        title="Anime"
+        name="One Piece"
+        link="www.zoro.bz" 
+      />
+      
+      <Cards
+        imgsrc={bleach}
+        imgalt="Bleach Image"
+        title="Anime"
+        name="Bleach"
+        link="www.zoro.bz" 
+      />
     </div>
   );
 }
---
+++ src/component/Cards.js
@@ -0,0 +1,15 @@
+export default function Cards(props) {
+    return (
+      <div className="cards">
+        <div className="card">
+          <img src={props.imgsrc} alt={props.imgalt}/>
+          <div className="card_info">
+            <p>{props.title}</p>
+            <h1>{props.name}</h1>
+            <a href={`https://${props.link}`} target="_blank" rel="noopener noreferrer">Watch Now</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
