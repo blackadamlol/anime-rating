095220165bc8918fbce89c4f77a7b382ad166e8b
blackadamlol <ssahilpreet35@gmail.com>
2023-07-09 22:09:07 +0100

    did some more css and now need to add more anime

--- src/App.js
+++ src/App.js
@@ -1,41 +1,23 @@
-import './App.css';
+import "./component/Cards.css";
 import React from 'react';
 import Cards from './component/Cards';
 import AnimeData from "./component/AnimeData"
+
 function App() {
   return (
     <div className="App">
-      <Cards
-        imgsrc={AnimeData[0].imgsrc}
-        imgalt={AnimeData[0].imgalt}
-        title={AnimeData[0].title}
-        name={AnimeData[0].name}
-        link={AnimeData[0].links}
-      />
-
-      <Cards
-        imgsrc={AnimeData[1].imgsrc}
-        imgalt={AnimeData[1].imgalt}
-        title={AnimeData[1].title}
-        name={AnimeData[1].name}
-        link={AnimeData[1].links}
-      />
-      
-      <Cards
-        imgsrc={AnimeData[2].imgsrc}
-        imgalt={AnimeData[2].imgalt}
-        title={AnimeData[2].title}
-        name={AnimeData[2].name}
-        link={AnimeData[2].links}
-      />
-
-      <Cards
-        imgsrc={AnimeData[3].imgsrc}
-        imgalt={AnimeData[3].imgalt}
-        title={AnimeData[3].title}
-        name={AnimeData[3].name}
-        link={AnimeData[3].links}
-      />
+      <div className="card-row"> 
+        {AnimeData.map((anime, index) => (
+          <Cards
+            key={index}
+            imgsrc={anime.imgsrc}
+            imgalt={anime.imgalt}
+            title={anime.title}
+            name={anime.name}
+            link={anime.links}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- src/component/Cards.js
+++ src/component/Cards.js
@@ -1,23 +1,22 @@
-import "./Cards.css"
+import "./Cards.css";
+
 export default function Cards(props) {
-    return (
-      <>
-        <div className="whole-webpage">
-          <div className="navbar">
-            <h1>Anime Recommending Website</h1>
-          </div>
-          <div className="cards">
-            <div className="card">
-              <img src={props.imgsrc} alt={props.imgalt}/>
-              <div className="card_info">
-                <p>{props.title}</p>
-                <h1>{props.name}</h1>
-                <a href={`https://${props.link}`} target="_blank" rel="noopener noreferrer">Watch Now</a>
-              </div>
-            </div>
-          </div>
+  return (
+    <div className="cards">
+      <div className="card">
+        <img src={props.imgsrc} alt={props.imgalt} />
+        <div className="card_info">
+          <p>{props.title}</p>
+          <h1>{props.name}</h1>
+          <a
+            href={`https://${props.link}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Watch Now
+          </a>
         </div>
-      </>
-    );
-  }
-  
+      </div>
+    </div>
+  );
+}
