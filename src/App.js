fb357461765b52cd46406e8d14f0f135b5a85cce
blackadamlol <ssahilpreet35@gmail.com>
2023-07-12 16:38:20 +0100

    need to modify searches

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import './App.css';
 import Cards from './component/Cards';
 import AnimeData from './component/AnimeData';
 import anifindlogo from "./images/anifind_logo.jpg";
-import Signup from './component/Signup';
-import Homepage from './component/Homepage';
-
+import searchIcon from "./images/search.png";
+import login from "./images/login.png"
 function App() {
+  const [searchVisible, setSearchVisible] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleToggleSearch = () => {
+    setSearchVisible(!searchVisible);
+  };
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredAnime = AnimeData.filter((anime) => {
+    return anime.title.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
   return (
     <div className="App">
-      <div className='logo'>
-        <Link to="/">
+      <div className="header">
+        <div className="logo">
           <img src={anifindlogo} alt="logo of website" />
-        </Link>
+        </div>
+        <div className="search-icon" onClick={handleToggleSearch}>
+          <img src={searchIcon} alt="search icon" />
+        </div>
+        <div className="signin">
+          <img src={login} alt="logo of sign in"/>
+        </div>
       </div>
+      {searchVisible && (
+        <div className="search-bar">
+          <input
+            type="text"
+            placeholder="Search anime"
+            value={searchTerm}
+            onChange={handleSearch}
+          />
+        </div>
+      )}
       <div className="card-row">
-        {AnimeData.map((anime, index) => (
+        {filteredAnime.map((anime, index) => (
           <Cards
             key={index}
             imgsrc={anime.imgsrc}
@@ -27,7 +56,6 @@ function App() {
           />
         ))}
       </div>
-      <Signup />
     </div>
   );
 }
