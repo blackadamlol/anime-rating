f145dbc8fe8edd22e9b405d7c9c25baea9af6be0
blackadamlol <ssahilpreet35@gmail.com>
2023-07-20 04:21:14 +0100

    made some css changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import Cards from './component/Cards';
 import AnimeData from './component/AnimeData';
 import anifindlogo from "./images/anifind_logo.jpg";
 import searchIcon from "./images/search.png";
-import login from "./images/login.png"
+import login from "./images/login.png";
+
 function App() {
   const [searchVisible, setSearchVisible] = useState(false);
   const [searchTerm, setSearchTerm] = useState('');
@@ -27,23 +28,25 @@ function App() {
         <div className="logo">
           <img src={anifindlogo} alt="logo of website" />
         </div>
-        <div className="search-icon" onClick={handleToggleSearch}>
-          <img src={searchIcon} alt="search icon" />
+        <div className="signin-search">
+          {searchVisible && (
+            <div className="search-bar">
+              <input
+                type="text"
+                placeholder="Search anime"
+                value={searchTerm}
+                onChange={handleSearch}
+              />
+            </div>
+          )}
+          <div className={`search-icon ${searchVisible ? 'active' : ''}`} onClick={handleToggleSearch}>
+            <img src={searchIcon} alt="search icon" />
+          </div>
         </div>
         <div className="signin">
-          <img src={login} alt="logo of sign in"/>
+          <img src={login} alt="logo of sign in" />
         </div>
       </div>
-      {searchVisible && (
-        <div className="search-bar">
-          <input
-            type="text"
-            placeholder="Search anime"
-            value={searchTerm}
-            onChange={handleSearch}
-          />
-        </div>
-      )}
       <div className="card-row">
         {filteredAnime.map((anime, index) => (
           <Cards
